d36683b3b8db62138b3948dc85a489300f92f7bd
Miguel Muñoz <munozmiguel321@gmail.com>
2020-02-20 08:40:08 +0000

    added picker and buttons to viewController

--- ChinesseZodiac/ViewController.swift
+++ ChinesseZodiac/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+//MARK: IBActions
+    
+    @IBAction func datePickerAction(_ sender: UIDatePicker) {
+    }
+   
+    @IBAction func selectDateButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 }
 
